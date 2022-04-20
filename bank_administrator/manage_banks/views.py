109d1d8d8ade12f26b302f5cd8d15906470fe3bf
Meyi96 <fabioandresmejia@hotmail.com>
2022-04-20 17:23:11 -0500

    first version for transfer not ready

--- bank_administrator/manage_banks/views.py
+++ bank_administrator/manage_banks/views.py
@@ -32,3 +32,19 @@ def charge_money(request, bankaccount_id):
     else:
         bankaccount.save()
         return HttpResponseRedirect(reverse("manage_banks:detail", args=(bankaccount.client.id,)))
+
+def transfer_money(request, bankaccount_id):
+    bankaccount = get_object_or_404(BankAccount,pk=bankaccount_id)
+    try:
+        account_to_transfer= get_object_or_404(BankAccount,pk=int(request.POST['account']))
+        amount= float(request.POST['amount']) 
+    except(ValueError, KeyError):
+        return render(request,"manage_banks/detail_bankAccount.html",{
+        'bankAccount':bankaccount,
+        'error_message_t': 'some of the data is uncompleted'
+        })
+    bankaccount.balance-=amount
+    account_to_transfer.balance+=amount
+    bankaccount.save()
+    account_to_transfer.save()
+    return HttpResponseRedirect(reverse("manage_banks:detail", args=(bankaccount.client.id,)))
