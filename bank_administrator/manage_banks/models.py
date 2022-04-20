3796f308f8634bec712e62e2f9d52c43e83f21ff
Meyi96 <fabioandresmejia@hotmail.com>
2022-04-20 10:35:15 -0500

    admin config and make models

--- bank_administrator/manage_banks/models.py
+++ bank_administrator/manage_banks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
+class Client(models.Model):
+    name=models.CharField(max_length=50)
+    age=models.IntegerField(validators=[MinValueValidator(18), MaxValueValidator(100)])
+
+class BankAccount(models.Model):
+    client=models.ForeignKey(Client, on_delete=models.CASCADE)
+    banckName= models.CharField(max_length=50)
+    accountNumber= models.CharField(max_length= 15)
+    balance=models.FloatField(default=0)
+    
+    """"Redondea balance en dos decimalestes de guardar en la DB"""
+    def save(self, *args, **kwargs):
+        self.balance = round(self.balance, 2)
+        super(BankAccount, self).save(*args, **kwargs)
+
